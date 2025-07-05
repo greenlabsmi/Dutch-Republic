6a0f32d6b78918383c7e734cdb5e251b1f19b950
greenlabsmi <shop@greenlabsmi.com>
2025-07-04 23:35:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -219,7 +219,7 @@ function renderDealsByCategory(data, selectedCategory) {
             ${d["Effects/Tagline"] ? `<p>${d["Effects/Tagline"]}</p>` : ''}
             ${d["Amount/Details"] ? `<p>${d["Amount/Details"]}</p>` : ''}
             ${d.Notes ? `<p class="deal-notes">${d.Notes}</p>` : ''}
-            p class="price-tag">$${d.Price} <span class="tax-note">tax included</span></p>
+            <p class="price-tag">$${d.Price} <span class="tax-note">tax included</span></p>
           </div>
         </div>
       `).join('')}
